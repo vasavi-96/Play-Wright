35ee5ecf7f44369964c49334539e637fc3cb3f4f
vasavi-96 <chamarthivasavi@gmail.com>
2026-03-12 14:42:58 +0530

    first commit

---
+++ tests/amazon.spec.js
@@ -0,0 +1,15 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('Amazon Search Product functionality', async ({ page }) => {
+  await page.goto('https://www.amazon.com/');
+  
+  // Search for products
+  await page.getByRole('searchbox', { name: 'Search Amazon' }).click();
+  await page.getByRole('searchbox', { name: 'Search Amazon' }).fill('watch');
+  await page.keyboard.press('Enter');
+  
+  // Verify search results are displayed
+  //await expect(page).toHaveURL(/.*search.*/);
+});
+
---
+++ tests/example.spec.js
@@ -0,0 +1,67 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('contact page title', async({page}) =>{
+  await page.goto('http://127.0.0.1:5501/contact.html');
+  await expect(page).toHaveTitle(/Pet Shop/);
+});
+
+test('contact page submit', async ({page}) => {
+  await page.goto('http://127.0.0.1:5501/contact.html');
+  const email = page.getByPlaceholder(/name@/);
+  const msg = page.locator('#exampleFormControlTextarea1');
+
+  const btn = page.getByText('Submit');
+
+  email.fill('aaryan@gmail.com');
+  msg.fill('asdfghjklk');
+
+  await btn.click();
+  await msg.screenshot({type: 'png', path: './msg.png'});
+  await btn.screenshot({type: 'png', path: './button.png'});
+  await page.screenshot({fullPage: true, type: 'png', path: './meow.png'});
+});
+
+[
+    {email: "vasavi@gmail.com", msg: "Hey Vass"},
+    {email: "chamarthi@gmail.com", msg: "Hey chamarthi"}
+].forEach(obj =>{
+
+    test(`contact html test for ${obj.email}`, async ({ page }) => {
+        await page.goto('http://127.0.0.1:5501/contact.html');
+        //await page.getByRole('textbox', { name: 'name@example.com' }).click();
+        //await page.getByRole('textbox', { name: 'name@example.com' }).fill(obj.email);
+
+        await page.locator('#exampleFormControlInput1').click();
+        await page.locator('#exampleFormControlInput1').fill(obj.email);
+
+        await page.locator('#exampleFormControlTextarea1').click();
+        await page.locator('#exampleFormControlTextarea1').fill(obj.msg);
+  
+  page.once('dialog', dialog => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
+  await page.getByRole('button', { name: 'Submit' }).click();
+});
+
+})
+
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+
