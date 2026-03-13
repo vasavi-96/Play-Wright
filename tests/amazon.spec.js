992e96c5f305e423e347be142459c3156832b8a3
vasavi-96 <chamarthivasavi@gmail.com>
2026-03-13 09:50:52 +0530

    message

--- tests/amazon.spec.js
+++ tests/amazon.spec.js
@@ -5,7 +5,7 @@ test('Amazon Search Product functionality', async ({ page }) => {
   await page.goto('https://www.amazon.com/');
   
   // Search for products
-  await page.getByRole('searchbox', { name: 'Search Amazon' }).click();
+  //await page.getByRole('searchbox', { name: 'Search Amazon' }).click();
   await page.getByRole('searchbox', { name: 'Search Amazon' }).fill('watch');
   await page.keyboard.press('Enter');
   
